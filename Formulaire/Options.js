8bb50c9c5e9efa18cdefcd451866d4af7f60965e
Le Cavalier Riant <130751738+le-cavalier-riant@users.noreply.github.com>
2024-11-07 16:17:45 +0100

    Add files via upload

---
+++ Formulaire/Options.js
@@ -0,0 +1,147 @@
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                     Héraldix                                                      //
+//                                                                                                                   //
+// ================================================================================================================= //
+
+titres = {
+	"Royaume de France": [
+		"Roi",
+		"Dauphin",
+		"Fils",
+		"Petit-fils",
+		"Prince",
+		"Duc (souverain)",
+		"Duc (non souverain)",
+		"Marquis et pair",
+		"Marquis",
+		"Comte et pair",
+		"Comte",
+		"Vicomte",
+		"Vidame",
+		"Baron",
+		"Banneret",
+		"Chevalier",
+		"Anobli",
+		"Bâtard",
+	],
+	"Royaume d'Espagne": [
+		"Roi d'Espagne",
+		"Grand",
+	],
+};
+
+écus = {
+	"": [
+		"Chavagnac",
+		"France",
+	],
+}
+
+couronnes = {
+	"Royaume de France": [
+		"Roi",
+		"Dauphin",
+		"Fils",
+		"Petit-fils",
+		"Prince",
+		"Duc et pair",
+		"Duc",
+		"Marquis et pair",
+		"Marquis",
+		"Comte et pair",
+		"Comte",
+		"Vicomte",
+		"Vidame",
+		"Baron",
+		"Banneret",
+		"Chevalier",
+	],
+	"Empire français": [
+		"Empereur (1804)",
+		"Empereur (1852)",
+		"Prince souverain impérial",
+		"Prince impérial",
+		"Duc impérial",
+		"Comte impérial",
+		"Baron impérial",
+		"Chevalier impérial",
+		"Grand dignitaire impérial",
+	],
+	"Villes françaises": [
+		"Capitale",
+		"Chef-lieu",
+		"Commune",
+	],
+	"Royaume d'Espagne": [
+		"Roi d'Espagne",
+		"Grand",
+	],
+};
+
+heaumes = {
+	"Royaume de France": [
+		"Roi",
+		"Prince",
+		"Duc",
+		"Marquis",
+		"Comte",
+		"Baron",
+		"Gentilhomme d'ancienne race",
+		"Gentilhomme de trois races",
+		"Anobli",
+		"Bâtard",
+	],
+};
+
+portants = {
+	"": [
+		"Ange",
+		"Lion",
+		"Ange (3/4)",
+		"Ange (profil)",
+		"Sauvage",
+		"Sauvage (pied sur sphère + bras tendu)",
+		"Guerrier africain",
+		"Hercules","Dauphin",
+		"Hyppocampe",
+		"Bélier",
+		"Lion rampant",
+		"Lion rampant (couronne britanique)",
+		"Tigre",
+		"Aigle",
+		"Griffon (or et sable)",
+		"Griffon (or et argent)",
+		"Griffon (or)",
+		"Lévrier",
+		"Wyverne (queue nouée)",
+		"Licorne",
+	],
+};
+
+colliers = {
+	"Royaume de France": [
+		"Ordre de Saint-Michel",
+		"Ordre du Saint-Esprit",
+	],
+	"Royaume d'Espagne": [
+		"Ordre de la Toison d'or",
+	],
+};
+
+manteaux = {
+	"Royaume de France": [
+		"France",
+		"Pair",
+		"Rouge",
+	],
+	"Royaume d'Espagne": [
+		"Grand",
+	],
+};
+
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                     Héraldix                                                      //
+//                                                                                                                   //
+// ================================================================================================================= //
