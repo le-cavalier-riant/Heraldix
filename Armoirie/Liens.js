afcc1f54de1f4dc2b4ed4151a4cc53a9d973b963
Le Cavalier Riant <artus2003chavagnac@gmail.com>
2024-11-16 15:45:54 +0100

    déplacement et renommage

---
+++ Armoirie/Liens.js
@@ -0,0 +1,420 @@
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                     Héraldix                                                      //
+//                                                                                                                   //
+// ================================================================================================================= //
+
+wikipédia = "https://upload.wikimedia.org/wikipedia/commons/";
+
+écus = {
+	"Royaume de France": {
+		"France": [
+			"Armoirie/Écus/France moderne A.svg",
+			"Armoirie/Écus/France moderne B.svg",
+			"Armoirie/Écus/France ancien.svg",
+			"Armoirie/Écus/France et Navarre.svg",
+			wikipédia + "1/13/Blason_France_moderne.svg",
+			wikipédia + "7/78/Proposed_Blason_for_Île-de-France.svg",
+			wikipédia + "9/95/Blason_pays_fr_France_ancien.svg",
+		],
+		"Chavagnac": [
+			"Armoirie/Écus/Chavagnac.png",
+			"Armoirie/https://data.man8rove.com/images/arms/full/1jo8ll90a-1.png",
+			"Armoirie/http://dechav.free.fr/png/Chavagnac_de_Champagne.png",
+			wikipédia + "d/d3/Blason_famille_Chavagnac_moderne.svg",
+			wikipédia + "d/d6/Blason_famille_fr_de_Chavagnac.svg",
+			wikipédia + "f/fb/Blason_famille_fr_de_Chavagnac_1.svg",
+			wikipédia + "0/06/Blason_famille_fr_de_Chavagnac_2.svg",
+		
+		],
+		"Breizh": [
+			wikipédia + "d/d8/Armoiries_Bretagne_-_Arms_of_Brittany.svg",
+			"https://fr.geneawiki.com/images/b/bb/Province_-_Blason_-_Bretagne.png",
+		],
+	},
+	"Empire français": {
+		"Bonaparte": [
+			wikipédia + "6/6e/Arms_of_the_French_Empire.svg",
+		],
+	},
+	"Villes françaises": {
+		"Paris": [
+			wikipédia + "c/ca/Blason_paris_75.svg",
+		],
+	},
+	"Reino de España": {
+		"España": [
+			wikipédia + "c/c7/Arms_of_Spain_(1977-1981).svg",
+		],
+	},
+	"United Kingdom": {
+		"Kingdom": [
+			wikipédia + "2/2b/Arms_of_the_United_Kingdom.svg",
+		],
+		"England": [
+			wikipédia + "1/1c/England_Arms_1405.svg",
+			wikipédia + "f/f7/Royal_arms_of_England.svg",
+			wikipédia + "2/28/England_COA.svg",
+			wikipédia + "1/10/Royal_Arms_of_England_(1340-1367).svg",
+			wikipédia + "8/86/Royal_Arms_of_England_(1399-1603).svg",
+		],
+		"Alba": [
+			wikipédia + "b/bd/Royal_Arms_of_the_Kingdom_of_Scotland.svg",
+		],
+	},	
+	"Heiliges Römisches Reich": {
+		"Heiliges Römisches Reich" : [
+			wikipédia + "4/43/Arms_of_the_Holy_Roman_Emperor_(c.1433-c.1450).svg",
+		],
+	},
+	"Status Civitatis Vaticanæ": {
+		"Civitas Vaticana": [
+			wikipédia + "3/3a/Coat_of_arms_of_Vatican_City_(2023–present).svg"
+		],
+	},
+	"Reino de Portugal": {
+		"Portugal": [
+			wikipédia + "6/6a/Royal_Arms_of_Portugal.svg",
+		],
+	},
+	"Groussherzogtum Lëtzebuerg": {
+		"Lëtzebuerg": [
+			wikipédia + "0/05/Arms_of_Luxembourg.svg",
+		],
+	},
+	"Konungariket Sverige": {
+		"Sverige": [
+			wikipédia + "a/a1/Shield_of_arms_of_Sweden.svg",
+		],
+	},
+}
+
+couronnes = {
+	"Royaume de France": {
+		"Roi": [
+			wikipédia + "1/1c/Royal_Crown_of_France_(accurate_red_bonnet).svg",
+			wikipédia + "3/3b/French_heraldic_crowns_-_King.svg",
+		],
+		"Dauphin": [
+			wikipédia + "7/7e/Crown_of_the_Dauphin_of_France_(variant).svg",
+			wikipédia + "e/e4/French_heraldic_crowns_-_Dauphin.svg",
+		],
+		"Fils": [
+			wikipédia + "6/6d/Crown_of_a_Royal_Prince_of_the_Blood_of_France.svg",
+		],
+		"Petit-fils": [
+			wikipédia + "1/1f/Crown_of_a_Royal_Prince_of_the_Blood_of_France_(variant).svg",
+		],
+		"Prince": [
+			wikipédia + "b/b7/Crown_of_a_Prince_of_the_Blood_of_France.svg",
+			wikipédia + "6/64/French_heraldic_crowns_-_Prince_de_sang_royal.svg",
+		],
+		"Duc et pair": [
+			wikipédia + "8/8e/Crown_of_a_Duke_of_France.svg",
+			wikipédia + "7/72/French_heraldic_crowns_-_duc_et_pair.svg",
+		],
+		"Duc": [
+			wikipédia + "d/d4/Crown_of_a_Duke_of_France_(variant).svg",
+			wikipédia + "c/c6/French_heraldic_crowns_-_duc_v2.svg",
+		],
+		"Marquis et pair": [
+			wikipédia + "e/e0/Crown_of_a_Marquis_of_France.svg",
+			wikipédia + "6/61/French_heraldic_crowns_-_marquis_et_pair.svg",
+		],
+		"Marquis": [
+			wikipédia + "7/7a/Crown_of_a_Marquis_of_France_(variant).svg",
+			wikipédia + "5/53/French_heraldic_crowns_-_marquis_v2.svg",
+		],
+		"Comte et pair": [
+			wikipédia + "b/b9/Crown_of_a_Count_of_France.svg",
+			wikipédia + "3/32/Heraldique_couronne_comte_et_pair.svg",
+		],
+		"Comte": [
+			wikipédia + "b/b1/Crown_of_a_Count_of_France_(variant).svg",
+			wikipédia + "1/1b/Heraldique_couronne_comte_françaisais.svg",
+		],
+		"Vicomte": [
+			wikipédia + "e/e8/Crown_of_a_Viscount_of_France_(variant).svg",
+			wikipédia + "6/63/Heraldique_couronne_vicomte_français.svg",
+		],
+		"Vidame": [
+			wikipédia + "0/03/Crown_of_a_Vidame_of_France.svg",
+			wikipédia + "e/e5/French_heraldic_crowns_-_Vidame.svg",
+		],
+		"Baron et pair": [
+			wikipédia + "archive/7/76/20100401071907!French_heraldic_crowns_-_baron_et_pair.svg",
+			wikipédia + "7/76/French_heraldic_crowns_-_baron_et_pair.svg",
+		],
+		"Baron": [
+			wikipédia + "b/b7/Crown_of_a_Baron_of_France.svg",
+			wikipédia + "6/66/French_heraldic_crowns_-_Baron.svg",
+		],
+		"Banneret": [
+			wikipédia + "3/31/Crown_of_a_Chevalier_of_France.svg",
+		],
+		"Chevalier": [
+			wikipédia + "6/6d/Torse_of_a_Chevalier_of_France.svg",
+			wikipédia + "2/23/French_heraldic_crowns_-_chevalier.svg",
+		],
+	},
+	"Empire français": {
+		"Empereur 1804": [
+			wikipédia + "4/41/Heraldic_Crown_of_the_First_French_Empire.svg",
+		],
+		"Empereur 1852": [
+			wikipédia + "6/68/Imperial_Crown_of_Napoleon.svg",
+		],
+		"Prince souverain": [
+			wikipédia + "e/e6/Crown_of_a_Napoleonic_Prince_Souverain.svg",
+		],
+		"Prince": [
+			wikipédia + "8/81/Cap_of_an_Imperial_Prince_Grand_Dignitary.svg",
+		],
+		"Duc": [
+			wikipédia + "5/5d/Cap_of_an_Imperial_Duke.svg",
+		],
+		"Comte": [
+			wikipédia + "6/62/Cap_of_an_Imperial_Count.svg",
+		],
+		"Baron": [
+			wikipédia + "a/ae/Cap_of_an_Imperial_Baron.svg",
+		],
+		"Chevalier": [
+			wikipédia + "4/40/Cap_of_an_Imperial_Knight.svg",
+		],
+		"Grand dignitaire": [
+			wikipédia + "7/7b/Bonnet_d`honneur.png",
+		],
+	},
+	"Villes françaises": {
+		"Capitale": [
+			wikipédia + "1/11/Mural_Crown_of_a_French_Capital.svg",
+		],
+		"Chef-lieu": [
+			wikipédia + "a/a2/Mural_Crown_of_a_French_City.svg",
+		],
+		"Commune": [
+			wikipédia + "a/aa/Mural_Crown_of_a_French_Commune.svg",
+		],
+	},
+	"Reino de España": {
+		"Rey": [
+			wikipédia + "1/1b/Heraldic_Royal_Crown_of_Spain_(Version_of_the_Royal_Arms).svg",
+		],
+		"Grande": [
+			wikipédia + "b/bd/Heraldic_Crown_of_Spanish_Grandee.svg",
+		],
+	},
+	"United Kingdom": {},
+	"Heiliges Römisches Reich": {},
+	"Status Civitatis Vaticanæ": {},
+	"Reino de Portugal": {
+		"Rei": [
+			wikipédia + "5/53/Heraldic_Crown_of_the_King_of_Portugal.png",
+		],
+	},
+	"Groussherzogtum Lëtzebuerg": {},
+	"Konungariket Sverige": {
+		"Kung": [
+			wikipédia + "9/99/Royal_crown_of_the_King_of_Sweden.svg",
+		],
+	},
+}
+
+heaumes = {
+	"Royaume de France": {
+		"Roi": [
+			"Armoirie/Heaumes/Roi-A.svg",
+			"Armoirie/Heaumes/Roi-B.png",
+			"Armoirie/Heaumes/Roi-C.png",
+			"Armoirie/Heaumes/Roi-D.png",
+		],
+		"Prince": [
+			"Armoirie/Heaumes/Prince-A.svg",
+			"Armoirie/Heaumes/Prince-B.png",
+			"Armoirie/Heaumes/Prince-C.png",
+		],
+		"Duc": [
+			"Armoirie/Heaumes/Duc-A.svg",
+			"Armoirie/Heaumes/Duc-B.png",
+			"Armoirie/Heaumes/Duc-C.png",
+		],
+		"Marquis": [
+			"Armoirie/Heaumes/Marquis-A.svg",
+			"Armoirie/Heaumes/Marquis-B.png",
+		],
+		"Comte": [
+			wikipédia + "0/0d/Meuble_héraldique_Heaume_comte.svg",
+			"Armoirie/Heaumes/Comte-A.png",
+			"Armoirie/Heaumes/Comte-B.png",
+			"Armoirie/Heaumes/Comte-C.png",
+		],
+		"Baron": [
+			"Armoirie/Heaumes/Baron-A.png",
+			"Armoirie/Heaumes/Baron-B.png",
+		],
+		"Gentilhomme d'ancienne race": [
+			"Armoirie/Heaumes/Gentilhomme d'ancienne race-A.png",
+			"Armoirie/Heaumes/Gentilhomme d'ancienne race-B.png",
+		],
+		"Gentilhomme de trois races": [
+			"Armoirie/Heaumes/Gentilhomme de trois races.png",
+			"Armoirie/Heaumes/Gentilhomme de trois races.jpg",
+		],
+		"Anobli": [
+			"Armoirie/Heaumes/Anobli-A.png",
+			"Armoirie/Heaumes/Anobli-B.png",
+		],
+		"Bâtard": [
+			"Armoirie/Heaumes/Bâtard-A.png",
+			"Armoirie/Heaumes/Bâtard-B.png",
+		],
+	},
+	"Empire français": {},
+	"Villes françaises": {},
+	"Reino de España": {},
+	"United Kingdom": {
+		"King": [
+			wikipédia + "e/e2/BarredHelmetGermanOrFF.svg",
+		],
+		"Baronet": [
+			wikipédia + "e/e9/Héraldique_ornement_Heaume_baronnet.svg",
+		],
+	},
+	"Heiliges Römisches Reich": {},
+	"Status Civitatis Vaticanæ": {},
+	"Reino de Portugal": {},
+	"Groussherzogtum Lëtzebuerg": {},
+	"Konungariket Sverige": {},
+}
+
+portants = {
+	"**": {
+		"Ange": [
+			"Armoirie/Portants/Ange.png",
+			wikipédia + "f/ff/Angelic_Supporter_(Heraldry).svg",
+			wikipédia + "6/68/Meuble_héraldique_Ange_debout.svg",
+		],
+		"Lion": [
+			"Armoirie/Portants/Lion.png",
+			wikipédia + "6/62/Lion_rampant_guardant_as_supporter_(Or).svg",
+			wikipédia + "6/66/Royal_Coat_of_Arms_of_the_United_Kingdom-Lion.svg",
+		],
+		"Sauvage": [
+			wikipédia + "9/91/Wildman_Supporter_(Heraldry).svg",
+			wikipédia + "7/7b/Wild_man_holding_club_with_one_foot_on_sphere.svg",
+		],
+		"Guerrier affricain": [
+			wikipédia + "f/f5/Meuble_guerrier_africain.svg",
+		],
+		"Hercules": [
+			wikipédia + "2/2f/Hercules_Supporter_(Heraldry).svg",
+		],
+		"Dauphin": [
+			wikipédia + "7/71/Dolphin_Supporter_(Heraldry).svg",
+		],
+		"Hyppocampe": [
+			wikipédia + "6/66/Ippocampo_Supporto_Sea_Horse_Supporter_(Heraldry).svg",
+		],
+		"Bélier": [
+			wikipédia + "d/d5/Ram_(Heraldic_Supporter).svg",
+		],
+		"Tigre": [
+			wikipédia + "5/52/Tiger_Supporter_(Heraldry).svg",
+		],
+		"Aigle": [
+			wikipédia + "1/10/Imperial_Eagle_Supporter_(Heraldry).svg",
+		],
+		"Griffon": [
+			wikipédia + "3/3c/Gold_Griffin_Supporter_(Heraldry).svg",
+			wikipédia + "5/51/Silver-Gold_Griffin_Supporter_(Heraldry).svg",
+			wikipédia + "1/1e/Sable-Gold_Griffin_Supporter_(Heraldry).svg",
+		],
+		"Lévrier": [
+			wikipédia + "9/97/Greyhound_Supporter_of_the_English_Heraldry.svg",
+		],
+		"Dragon": [
+			wikipédia + "c/c5/Héraldique_meuble_Dragon_(wyvern).svg",
+			wikipédia + "0/0b/Meuble_héraldique_Dragon_(wyvern).svg",
+		
+		],
+		"Licorne": [
+			wikipédia + "9/96/Unicorn_Supporter_(Heraldry).svg",
+		],
+	},
+}
+
+colliers = {
+	"**": {
+		"Corde": [
+			wikipédia + "1/1e/Heraldic_cord.svg",
+		],
+	},
+	"Royaume de France": {
+		"Ordre de Saint-Michel": [
+			wikipédia + "5/53/Order_of_Saint_Michael_(heraldry).svg",
+		],
+		"Ordre du Saint-Esprit": [
+			wikipédia + "3/34/Order_of_the_Holy_Spirit_(heraldry).svg",
+		],
+	},
+	"Empire français": {},
+	"Villes françaises": {},
+	"Reino de España": {
+		"Orden del Toisón de Oro": [
+			"Armoirie/Colliers/Ordre de la Toison d'or.svg",
+		],
+	},
+	"United Kingdom": {},
+	"Heiliges Römisches Reich": {},
+	"Status Civitatis Vaticanæ": {},
+	"Reino de Portugal": {},
+	"Groussherzogtum Lëtzebuerg": {},
+	"Konungariket Sverige": {},
+}
+
+manteaux = {
+	"Royaume de France": {
+		"France": [
+			"Armoirie/Manteaux/France.svg",
+		],
+		"Pair": [
+			wikipédia + "a/a7/Blue_heraldic_mantle.svg",
+			wikipédia + "b/b6/Meuble_héraldique_manteau_01.svg",
+		],
+		"Rouge": [
+			wikipédia + "0/06/Generic_mantle.svg",
+		],
+	},
+	"Reino de España": {
+		"Grande": [
+			wikipédia + "4/4f/Manto_de_Grande_de_España.svg",
+		],
+	},
+}
+
+cimiers = {
+	"**": {
+		"Bassinet" : [
+			wikipédia + "a/aa/Bassinet.SVG",
+		],
+	},
+}
+
+/*
+	%21			!
+	%28			(
+	%29			)
+	%60			`
+	%C3%8E		Î
+	%C3%A7		ç
+	%C3%A9		é
+	%C3%B1		ñ
+	%E2%80%93	–
+*/
+
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                     Héraldix                                                      //
+//                                                                                                                   //
+// ================================================================================================================= //
